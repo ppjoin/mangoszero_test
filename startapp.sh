9d8b4a47ae3d7e975e47e3cbd936c06bad703053
ppjoin <38512728+ppjoin@users.noreply.github.com>
2018-04-21 21:27:46 +0800

    Update startapp.sh

--- startapp.sh
+++ startapp.sh
@@ -5,13 +5,15 @@ chmod 777 /config/*
 rm -rf /config/etc/realmd.conf
 cp -r /config/etc/realmd.conf.dist /config/etc/realmd.conf
 sed -i "s/127.0.0.1;3306;mangos;mangos;realmd/${MYSQL_ADDRESS};${MYSQL_PORT};${MYSQL_USER};${MYSQL_PASSWD};${MYSQL_REALMD_DBNAME};/g" /config/etc/realmd.conf
-/config/bin/realmd -c /config/etc/realmd.conf &
+# /config/bin/realmd -c /config/etc/realmd.conf &
+cd /config/bin/ && ./realmd &
 sleep 10
+sed -i '/AiPlayerbot.Enabled /c AiPlayerbot.Enabled = 0' /config/etc/mangosd.conf
 rm -rf /config/etc/mangosd.conf
 cp -r /config/etc/mangosd.conf.dist /config/etc/mangosd.conf
 sed -i "s/127.0.0.1;3306;root;mangos;realmd/${MYSQL_ADDRESS};${MYSQL_PORT};${MYSQL_USER};${MYSQL_PASSWD};${MYSQL_REALMD_DBNAME};/g" /config/etc/mangosd.conf
 sed -i "s/127.0.0.1;3306;root;mangos;mangos0/${MYSQL_ADDRESS};${MYSQL_PORT};${MYSQL_USER};${MYSQL_PASSWD};${MYSQL_WORLD_DBNAME};/g" /config/etc/mangosd.conf
 sed -i "s/127.0.0.1;3306;root;mangos;character0/${MYSQL_ADDRESS};${MYSQL_PORT};${MYSQL_USER};${MYSQL_PASSWD};${MYSQL_CHARACTER_DBNAME};/g" /config/etc/mangosd.conf
 sed -i '/DataDir                      = /c DataDir                      = "/config/data"' /config/etc/mangosd.conf
-/config/bin/mangosd -c /config/etc/mangosd.conf
-sleep 6000000000000000
+cd /config/bin/ && ./mangosd
+sleep 60
